efd14a08a36a16cd10d1ed6cb774bc9744615fd1
Pang Jia Mei <josiejmpang@gmail.com>
2021-03-18 20:36:06 +0800

    Initial Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,23 @@
+package samples.demo;
+
+public class Location {
+	
+	/**
+	 * Declaration of attribute
+	 */
+	private String city;
+
+	/**
+	 * Setter for city
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	/**
+	 * Getter for city
+	 */
+	public String getCity() {
+		return city;
+	}
+}
